99f03034881ded6c05da8d52cdf7b01be1b57b88
URLbug <gemslove2@gmail.com>
2024-03-27 22:07:59 +0200

    not news

---
+++ local/modules/the_modulare/install/components/modular.users/lang/ru/.description.php
@@ -0,0 +1,4 @@
+<?php
+
+$MESS['MODULAR_USERS_NAME'] = 'Группа пользователй';
+$MESS['MODULAR_USERS_DESC'] = 'Компонент для вывода групп пользователей';
---
+++ local/modules/the_modulare/install/components/modular/templates/.default/modular/modular.detail/.parameters.php
@@ -0,0 +1,14 @@
+<?php
+
+if(!defined("B_PROLOG_INCLUDED") || !B_PROLOG_INCLUDED) die();
+
+
+$arComponentParameters = array(
+    'PARAMETERS' => array(
+        'ID' => [
+            'PARENT' => 'BASE',
+            'NAME' => GetMessage('ID'),
+            'TYPE' => 'INT',
+        ],
+    ),
+);
